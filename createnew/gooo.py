7c81b9cb7ce521aee3bf04df7bf776f2196239d5
cangyunye <cangyunye@163.com>
2018-03-25 23:07:12 +0800

    commit

--- createnew/gooo.py
+++ createnew/gooo.py
@@ -1,10 +1,16 @@
-from collections import Counter
-"""Counter是一个简单的计数器，例如，统计字符出现的个数"""
+import base64
+"""base64编码
+    对于给出的二进制数据(字节)，按顺序每3个字节划分为1组，同时每个字节为8bit，所以共3 x 8 =24bit, 将24bit 划分为4组，即每组6位
+    那么每组就有2^6=64种可能性，编制一个base64编码表，每组的数值对应从编码表查询出的码。即每三个字符有4个码，
+    遇到给出的字符串非3的倍数，直接尾部补\x00以及1个或2个==,表示补了多少字节
+"""
 
 def main():
-    c = Counter()
-    for ch in 'programming':
-        c[ch] = c[ch] + 1
-    print(c)
+    string = 'Glad,My Half'.encode('ascii') #编码为ascii 字节数据
+    string2 = '黎明'.encode('utf-8') #中文用utf-8编码为 字节数据
+    encrypt=base64.b64encode(string)
+    print(encrypt)
+    decrypt = base64.b64decode(encrypt)
+    print(decrypt)
 if __name__ == '__main__':
     main()
---
+++ data structure/base64_encodeco.py
@@ -0,0 +1,24 @@
+import base64
+"""base64编码
+          对于给出的二进制数据(字节)，按顺序每3个字节划分为1组，同时每个字节为8bit，所以共3 x 8 =24bit, 将24bit 划分为4组，即每组6位
+    那么每组就有2^6=64种可能性，编制一个base64编码表，每组的数值对应从编码表查询出的码。即每三个字符有4个码，
+    遇到给出的字符串非3的倍数，直接尾部补\x00以及1个或2个==,表示补了多少字节
+         由于标准的Base64编码后可能出现字符+和/，
+    在URL中就不能直接作为参数，所以又有一种"url safe"的base64编码，其实就是把字符+和/分别变成-和_
+    base64.urlsafe_b64encode(b'i\xb7\x1d\xfb\xef\xff')
+    base64.urlsafe_b64decode('abcd--__')
+"""
+
+
+def main():
+    string = 'Glad,My Half'.encode('ascii')  # 编码为ascii 字节数据
+    string2 = '黎明'.encode('utf-8')  # 中文用utf-8编码为 字节数据
+    encrypt = base64.b64encode(string) 
+    print(encrypt)
+    decrypt = base64.b64decode(encrypt)
+    print(decrypt)
+
+
+
+if __name__ == '__main__':
+    main()
