f2d823a38815ee9a43ec21f35343b5cb7f56eda3
cangyunye <cangyunye@163.com>
2018-05-07 08:19:45 +0800

    modify list

--- data structure/list.py
+++ data structure/list.py
@@ -20,3 +20,14 @@ l1.append(11)
 print("After l1.append(11)\n",l1)
 print("list ( range (1,31) )\n",l3)
 print('2 in l2=','2' in l2)
+"""
+列表推导式（list comprehensions）
+列表推导式（又称列表解析式）提供了一种简明扼要的方法来创建列表。
+它的结构是在一个中括号里包含一个表达式，然后是一个for语句，然后是0个或多个for或者if语句。那个表达式可以是任意的，意思是你可以在列表中放入任意类型的对象。返回结果将是一个新的列表，在这个以if和for语句为上下文的表达式运行完成之后产生。
+规范
+"""
+variable = [out_exp for out_exp in input_list if out_exp == 2]
+# 这里是另外一个简明例子:
+multiples = [i for i in range(30) if i % 3 is 0]
+print(multiples)
+# Output: [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
