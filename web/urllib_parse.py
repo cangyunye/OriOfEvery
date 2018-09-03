fa894aafb955d711926f45bade346f530b79a019
cangyunye <cangyunye@163.com>
2018-09-03 23:19:06 +0800

    增加quote、unquote，中文与URL编码转化

--- web/urllib_parse.py
+++ web/urllib_parse.py
@@ -13,3 +13,12 @@ res3 = urlunparse(('https','www.baidu.com','index.html','user','a=7','comment'))
 print(type(res),res)
 print(type(res2),res2)
 print(type(res3),res3)
+#将中文编码转化为URL编码
+from urllib.parse import quote
+keyword = '壁纸'
+url= 'https://www.baidu.com/s?wd='+quote(keyword)
+print(url)
+#对URL进行解码
+from urllib.parse import unquote
+url = 'https://www.baidu.com/s?wd=%E5%A3%81%E7%BA%B8'
+print(unquote(url))
