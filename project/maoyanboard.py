dcff484f3ebc0ee149c2eedbfa0d174c79a73571
cangyunye <cangyunye@163.com>
2018-10-01 22:00:52 +0800

    补充完整100名排行榜

--- project/maoyanboard.py
+++ project/maoyanboard.py
@@ -1,4 +1,4 @@
-import re,requests,json
+import re,requests,json,time
 
 def parse_html(url):
     #请求头，必须加，不然无法爬取
@@ -7,7 +7,7 @@ def parse_html(url):
     }
     board_get = requests.get(url,headers=headers)
     #榜单列表，榜单列表区根据当前网页直接设定，也可在board页面找到特征，取href进行拼接
-    pattern_board = re.compile('<i class="board-index board-index-\d">(.*?)</i>.*?<img data-src="(.*?)" alt=.*?class="movie-item-info".*?<a href="(.*?)" title=".*?" data-act="boarditem-click" data-val="(.*?)">(.*?)</a>.*?class="star">\s*(.*?)\s*?</p>.*?<p class="releasetime">(.*?)</p>.*?<p class="score"><i class="integer">(.*?)</i><i class="fraction">(.*?)</i>',re.S)
+    pattern_board = re.compile('<i class="board-index board-index-\d*">(.*?)</i>.*?<img data-src="(.*?)" alt=.*?class="movie-item-info".*?<a href="(.*?)" title=".*?" data-act="boarditem-click" data-val="(.*?)">(.*?)</a>.*?class="star">\s*(.*?)\s*?</p>.*?<p class="releasetime">(.*?)</p>.*?<p class="score"><i class="integer">(.*?)</i><i class="fraction">(.*?)</i>',re.S)
     board_lists = re.findall(pattern_board,board_get.text)
     return board_lists
 
@@ -27,15 +27,19 @@ def return_dict(board_lists):
         
 #写入文件
 def write_to_file(board_dict):
-    with open('board_dict.txt','wt',encoding='utf-8') as f:
+    with open('board_dict.txt','a',encoding='utf-8') as f:
         for line in board_dict:
             #write只能写str，所以使用dumps解析字典成str，如果不使用ensure_ascii=False，则会将中文输出二进制数据，+"\n"完成换行
             f.write(json.dumps(line,ensure_ascii=False)+"\n")
 
-def main():
+def main(page):
     #猫眼看板地址
-    maoyan_board = 'http://maoyan.com/board/4'
+    offset='?offset={}'.format(page*10)
+    maoyan_board = 'http://maoyan.com/board/4'+offset
     #图个方便写一起了
     write_to_file(return_dict(parse_html(maoyan_board)))
 if __name__ == '__main__':
-    main()
+    for num in range(0,10,1):
+        main(num)
+        time.sleep(1)
+
