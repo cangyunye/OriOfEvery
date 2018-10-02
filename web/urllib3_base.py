4ca2de6a59ffe8a023c6b6d6f1bcf5b347f425fe
cangyunye <cangyunye@163.com>
2018-10-02 22:54:59 +0800

    目录结构调整

--- web/urllib3_base.py
+++
@@ -1,8 +0,0 @@
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-import urllib3
-http = urllib3.PoolManager()
-website='https://www.python.org/'
-
-r = http.request('GET',website)
-print(r.data)
