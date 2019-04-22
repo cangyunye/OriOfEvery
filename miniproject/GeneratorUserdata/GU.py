cbb737645a52d5886d58240707285e2bb2eab7e0
cangyunye <cangyunye@163.com>
2019-04-22 23:35:17 +0800

    启动器参数配置

--- miniproject/GeneratorUserdata/GU.py
+++ miniproject/GeneratorUserdata/GU.py
@@ -15,51 +15,56 @@ __description__ = textwrap.dedent('''\
 
 __version__ = "GU.0.0.2"
 parser = argparse.ArgumentParser(description=__description__, formatter_class=argparse.RawDescriptionHelpFormatter,
-								 prog='UserDataGenerator', epilog="Nowhere to be seen.")
-#argument:servnumber,region,cfgfile
+                                 prog='UserDataGenerator', epilog="Nowhere to be seen.")
+# argument:servnumber,region,cfgfile
 parser.add_argument('servnumber', metavar='servnumber', type=str,
-					help='The 11 integer for the servnumber.')
+                    help='The 11 integer for the servnumber.')
 
-parser.add_argument('region', metavar='region', type=str,
-					help='The region of servnumber.')
+parser.add_argument('mode', metavar='mode', type=str,
+                    help='The mode of implement or recreate.')
 
-parser.add_argument('-c','--config', action='append',dest='cfg',nargs='+',
-					help='Specific business config.')
+parser.add_argument('-c', '--config', action='append', dest='cfg', default='GlobalSettings.ini',
+                    help='Specific global variables setting configfile.')
 
-#addtional info
-parser.add_argument('-b','--brand', required=False, type=str,choices=['szx','SZX','gt','GT'],
-					help='Brand like szx as BrandSzx or gt as BrandGotone.')
-parser.add_argument('-a','--active', required=False, type=str,
-					help='If this servnumber activated.')
-parser.add_argument('-r','--route', required=False, type=str,
-					help='If route info is exists.')
-parser.add_argument('-e','--env', required=False, type=str,choices=['crm','CRM','test','TEST'],
-					help='Environment like crm as or test.')
+# addtional info
+parser.add_argument('-r', '--region', required=False, type=str,
+                    help='Setting the region of servnumber.')
+parser.add_argument('-sc', '--scfg', required=False, type=str,
+                    help='Self clean  json file as sscripts.')
+parser.add_argument('-bc', '--bcfg', required=False, type=str,
+                    help='Business json file as scripts.')
+parser.add_argument('-b', '--brand', required=False, type=str,
+                    help='Brand of servnumber')
+parser.add_argument('-a', '--active', required=False, type=str,
+                    help='If this servnumber activated.')
+parser.add_argument('-n', '--nodeid', required=False, type=str,
+                    help='Nodeid of servnumber.')
 
-#add_help
-parser.add_argument('-v','--version', help='version:%s' % (__version__))
-parser.add_argument('-d','--debug',action='store_true',help='show about debug',default=False)
+# add_help
+parser.add_argument('-v', '--version', help='version:%s' % (__version__))
+parser.add_argument('-d', '--debug', action='store_true',
+                    help='show about debug', default=False)
 
 
 args = parser.parse_args()
 
 
 def main():
-	param = {
-		'servnumber':args.servnumber if args.servnumber  else None,
-		'mode':args.mode,
-		'region':args.region if args.region  else None,
-		'scfg':args.scfg if args.scfg  else None,
-		'bcfg':args.bcfg if args.bcfg  else None,
-		'brand':args.brand if args.brand  else None,
-		'brand':args.brand if args.brand  else None,
-		'active':args.active if args.active  else None,
-		'nodeid':args.nodeid if args.nodeid  else None,
-		'cfgfile':args.cfg if args.cfg  else 'GlobalSettings.ini'
-	}
-	print(param)
-	g=UserDataGenerator(args.servnumber)
-	g.process(**param)
+    param = {
+        'servnumber': args.servnumber if args.servnumber else None,
+        'mode': args.mode,
+        'region': args.region if args.region else None,
+        'scfg': args.scfg if args.scfg else None,
+        'bcfg': args.bcfg if args.bcfg else None,
+        'brand': args.brand if args.brand else None,
+        'active': args.active if args.active else None,
+        'nodeid': args.nodeid if args.nodeid else None,
+        'cfgfile': args.cfg if args.cfg else 'GlobalSettings.ini'
+    }
+    print(param)
+    g = UserDataGenerator(args.servnumber)
+    g.process(**param)
+
 
 if __name__ == "__main__":
-	main()
+    main()
