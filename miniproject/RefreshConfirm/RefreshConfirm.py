2cb4426d869cf59d39a9b4e0ca6f3c2f2b539240
cangyunye <cangyunye@163.com>
2020-01-09 08:38:09 +0800

    优化时间格式处理，修复excel读取错误

--- miniproject/RefreshConfirm/RefreshConfirm.py
+++ miniproject/RefreshConfirm/RefreshConfirm.py
@@ -5,7 +5,8 @@ import paramiko
 from csv import DictReader
 from openpyxl import load_workbook
 from pathlib import PurePath
-from time import strptime,mktime
+# from time import strptime, mktime
+# from datetime import datetime
 import re
 
 # 版本更新
@@ -21,7 +22,7 @@ USERNAME = sshinfo['username']
 PASSWORD = sshinfo['password']
 
 # 此处要主要路径转换为绝对路径不能使用~
-downloadlist = [] # (remotefile,localfile)
+downloadlist = []  # (remotefile,localfile)
 remotepath = report['remotepath']
 localpath = report['localpath']
 # 调用脚本执行命令
@@ -35,7 +36,7 @@ class Data_S():
 	FilePath: str = '修改文件'
 	Revision: int = '版本'
 	ChangedAuthor: str = '变更人员'
-	ChangedDate: str = 'yyyy/mm/dd hh24:mi:ss' # strptime(ChangedDate, '%Y-%m-%d %H:%M:%S')
+	ChangedDate: str = 'yyyy-mm-dd hh24:mi:ss'  # strptime(ChangedDate, '%Y-%m-%d %H:%M:%S')
 
 
 # 定义execleOut协议
@@ -46,7 +47,8 @@ class Data_E():
 	FilePaths: str = '文件列表'
 	FileType: str = '交付件'
 	ChangedAuthor: str = '开发人员'
-	ChangedDate: str = 'yyyy/mm/dd hh24:mi:ss'
+	# 如果B列为日期格式，那么会自动转换为datetime.datetime对象
+	ChangedDate: str = 'yyyy-mm-dd hh24:mi:ss'
 	Status: str = '回归情况'
 
 
@@ -64,15 +66,20 @@ def filematcher(tbd):
 
 def compare(S, E):
 	# 遍历excel数据
-	for iE in E:
-		# 排除已完成的部分
-		if iE.Status == '测试回归完成':
-			continue
+	# excelOutList[1].ChangedDate.__rsub__(datetime.now()).seconds <= 3600
 
-	# TODO 分类比较 if re.search('BIN',iE.FileType)
-	# 对于BIN
-	# 对于JAR/WAR
-	# 对于脚本/配置
+# TODO 分类比较 if re.search('BIN',iE.FileType)
+# 对于BIN
+# 对于JAR/WAR
+# 对于脚本/配置
+
+def retrieveAppend(command,ssh):
+	stdin, stdout, stderr = ssh.exec_command(command)
+	_file = stdout.read()
+	remotefile = _file
+	# 过滤为仅文件名不带目录
+	localfile = PurePath(localpath, _file).name
+	downloadlist.append((remotefile, localfile))
 
 def main():
 	# 打印配置信息
@@ -83,11 +90,8 @@ def main():
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(HOSTNAME, port=PORT, username=USERNAME, password=PASSWORD)
 	# 调用后台shell脚本，生成shellOut文件
-	stdin,stdout,stderr = ssh.exec_command(command)
-	_file = stdout.read()
-	remotefile = _file
-	localfile = PurePath(localpath,_file).name
-	downloadlist.append((remotefile,localfile))
+	retrieveAppend(command, ssh)
+
 	# 方式1：下载文件
 	try:
 		trans = ssh.get_transport()
@@ -114,18 +118,22 @@ def main():
 	# filter
 	# 读取刷包excel，保存到Data_E对象
 	excelOutList = []
-	wb = load_workbook('刷包.xlsx')
-	sheet = wb.get_sheet_by_name('Sheet1')
+	wb = load_workbook(localpath['excelpath'])
+	sheet = wb.get_sheet_by_name('Sheet')
 	for row in map(str, range(2, sheet.max_row + 1)):
-		for line in sheet[row]:
-			excelOutList.append(Data_E(sheet[f'A{row}'],sheet[f'F{row}'], sheet[f'E{row}'],sheet[f'G{row}'], sheet[f'B{row}'], sheet[f'L{row}']))
+		if sheet[f'L{row}'].value == '测试回归完成' or sheet[f'L{row}'].value == None:
+			continue
+		excelOutList.append(
+			Data_E(sheet[f'A{row}'].value, sheet[f'F{row}'].value, sheet[f'E{row}'].value, sheet[f'G{row}'].value,
+				   sheet[f'B{row}'].value, sheet[f'L{row}'].value))
+	wb.close()
 
-	# 分析shellOut和excelparse，并输出结果到pythonOut
+# 分析shellOut和excelparse，并输出结果到pythonOut
 
-	# parseOut()
+# parseOut()
 
 
 if __name__ == "__main__":
 	main()
 
-	# schedule.every(30).seconds.do(print('1'))
+# schedule.every(30).seconds.do(print('1'))
