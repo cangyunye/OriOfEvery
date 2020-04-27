b457ec5f2b499b740bfe575fa727ee1590aeb895
cangyunye <cangyunye@163.com>
2020-04-27 23:22:18 +0800

    更新网站图标

--- DjangoVio/Vio/navi/urls.py
+++ DjangoVio/Vio/navi/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'navi'
 
 urlpatterns = [
-	path('',views.hi,name='hi'),
+	path('',views.index,name='hi'),
 	path('msgb/',views.msgb,name='hi'),
 	path('msgb/save/',views.msgsave,name='save'),
-	path('wttr/',views.wttr,name='save')
+	path('wttr/',views.wttr,name='save'),
+	path('jcount/',views.viscount,name='jcount')
 
 ]
--- DjangoVio/Vio/navi/views.py
+++ DjangoVio/Vio/navi/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render,HttpResponse
-from .models import msgboard
+from django.http import JsonResponse
 from django.views.decorators.clickjacking import xframe_options_exempt
+from .models import MsgBoard,VisitorInfo
 from datetime import datetime
 import requests
 
 # Create your views here.
-def hi(request):
+def index(request):
 	if request.method == 'GET':
-		msg = msgboard.objects.order_by('msgdate').reverse()[:5]
-		context = {"messages":msg}
+		if request.META.get('HTTP_X_FORWARDED_FOR'):
+			ip = request.META['HTTP_X_FORWARDED_FOR']
+		else:
+			ip = request.META['REMOTE_ADDR']
+		vis = VisitorInfo(visitor=ip,visdate=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+		vis.save()
+		msg = MsgBoard.objects.order_by('msgdate').reverse()[:5]
+		context = {"messages":msg,"ip":ip}
 		return render(request,"navi/index.html",context=context)
 
 @xframe_options_exempt
 def msgb(request):
 	if request.method == 'GET':
-		msg = msgboard.objects.all()
+		msg = MsgBoard.objects.all()
 		context = {"messages":msg}
 		return render(request,"navi/messageboard.html",context=context)
 
@@ -26,9 +33,9 @@ def msgsave(request):
 		return HttpResponse("naughty you are.lol ")
 	else:
 		msgdate = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		model = msgboard(visitor=visitor,msgdate=msgdate,email=email,message=message)
+		model = MsgBoard(visitor=visitor, msgdate=msgdate, email=email, message=message)
 		model.save()
-		return HttpResponse(f'msgboard<visitor = {visitor},msgdate = {msgdate},email = {email}\n {message}')
+		return HttpResponse(f'MsgBoard<visitor = {visitor},msgdate = {msgdate},email = {email}\n {message}')
 
 def wttr(request):
 	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
@@ -37,3 +44,13 @@ def wttr(request):
 	# 需要解决远端网站的css调用
 	text = req.text.replace(r"/files/style.css",r"http://wttr.in/files/style.css")
 	return HttpResponse(text)
+
+def viscount(request):
+	"""
+	:param request:
+	:return: json格式的访问统计index页统计
+	"""
+	vistoday = VisitorInfo.objects.filter(visdate__gte=datetime.now().strftime("%Y-%m-%d")).count()
+	vishisday = VisitorInfo.objects.count()
+	return JsonResponse({"today":vistoday,
+						 "history":vishisday})
