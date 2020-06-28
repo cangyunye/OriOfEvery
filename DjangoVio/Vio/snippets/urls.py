1fce947bb84099cb4030f260c87e8fedbbdfe3d6
cangyunye <cangyunye@gmail.com>
2020-06-29 07:56:36 +0800

    class based views

--- DjangoVio/Vio/snippets/urls.py
+++ DjangoVio/Vio/snippets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('',views.snippet_list),
-    path('<int:pk>/',views.snippet_detail),
+    path('',views.SnippetList.as_view()),
+    path('<int:pk>/',views.SnippetDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
